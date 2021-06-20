e4d3f219e9e9989c8201d8e3438c0f0022031f66
LENOVO <fayzahnouch6@gmail.com>
2021-06-20 16:35:20 +0200

    hhh

--- src/app/achats/achats.component.ts
+++ src/app/achats/achats.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {AchatsService} from "../controller/service/achats.service";
 import {Achats} from "../controller/model/achats.model";
-import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap'
+
 
 @Component({
   selector: 'app-achats',
